111d497ab14a6c3ad356f6a92cc526d8e7b1808a
Mile Lale <65227363+milelale@users.noreply.github.com>
2024-10-24 21:08:57 +0200

    Updated main.cpp

--- 6th project/main.cpp
+++ 6th project/main.cpp
@@ -4,10 +4,12 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <map>
+
 using namespace std;
-
 bool error = false;
-
+int varCounter = 16;
+string errorMsg = "";
 void writeFile(ifstream & File){
     string line;
     cout << "file content:\n";
@@ -15,6 +17,7 @@ void writeFile(ifstream & File){
         cout << line << endl;
     }
 }
+
 string removeWhiteSpace(string str){
     string newStr;
     for(int i = 0;i < str.length();i++){
@@ -24,29 +27,30 @@ string removeWhiteSpace(string str){
     }
     return newStr;
 }
+
 string DecToBinary(int num){
     if(num == 0){
         return "0";
     }
-	string bin = "";
-	while (num > 0){	
-		bin = (num % 2== 0 ? "0" : "1") + bin;
-		num/=2;
-	}
-	return bin;
+    string bin = "";
+    while (num > 0){    
+        bin = (num % 2== 0 ? "0" : "1") + bin;
+        num/=2;
+    }
+    return bin;
 }
+
 string shiftToRight(string str){
     string shiftedStr = "";
     shiftedStr.resize(str.size());
     char last = str[str.size()-1];
-
     for(int i = 0;i< str.size();i++){
         shiftedStr[i+1] = str[i];
     }
-
     shiftedStr[0] = last;
     return shiftedStr;
 }
+
 bool containsSymbol(char symbol, string str){
     for(int i = 0;i < str.size();i++){
         if(str[i] == symbol){
@@ -55,25 +59,7 @@ bool containsSymbol(char symbol, string str){
     }
     return false;
 }
-void Ainstruction(int currLine, vector <string> & instructions, vector <string> & binaryInstructions){
-    
-    string binaryInstructionTemplate = "0000000000000000";
-    instructions[currLine][0] = ' ';
-    string binInstruction = DecToBinary(stoi(instructions[currLine]));
 
-    for(int i = binInstruction.size()-1; i >= 0;i--){
-        binaryInstructionTemplate[i] = binInstruction[i];
-    }
-    int inscSize = 16-binInstruction.size();
-    while (inscSize>0)
-    {
-        binaryInstructionTemplate = shiftToRight(binaryInstructionTemplate);
-        inscSize--;
-    }
-    
-    binaryInstructions[currLine] = binaryInstructionTemplate;
-    //cout << "A instruction:"<< binaryInstructionTemplate << endl;
-}
 string jmpToBit(string jmp){
     jmp[3] = ' ';
     if(jmp == "JMP"){
@@ -101,6 +87,7 @@ string jmpToBit(string jmp){
         return "000";
     }
 }
+
 string cmpToBit(string cmp){
     if(cmp == "0"){
         return "0101010";
@@ -187,16 +174,40 @@ string cmpToBit(string cmp){
         return "1010101";
     }
     else{
-        return "000000";
         error = true;
+        errorMsg + "Los c";
+        return "000000";
     }
-
 }
+
+bool existsInMap(string key, map<string,int> & symbolTable){
+    auto it = symbolTable.find(key);
+    if (it != symbolTable.end()) {return true;} 
+    else {return false;}
+}
+
+void Ainstruction(int currLine, vector <string> & instructions, vector <string> & binaryInstructions){
+    
+    string binaryInstructionTemplate = "0000000000000000";
+    instructions[currLine][0] = ' ';
+    string binInstruction = DecToBinary(stoi(instructions[currLine]));
+    for(int i = binInstruction.size()-1; i >= 0;i--){
+        binaryInstructionTemplate[i] = binInstruction[i];
+    }
+    int inscSize = 16-binInstruction.size();
+    while (inscSize>0)
+    {
+        binaryInstructionTemplate = shiftToRight(binaryInstructionTemplate);
+        inscSize--;
+    }
+    
+    binaryInstructions[currLine] = binaryInstructionTemplate;
+}
+
 void Cinstruction(int currLine, vector <string> & instructions, vector <string> & binaryInstructions){
     string binaryInstructionTemplate = "1110000000000000";
     string formula = "";
     string instruction = instructions[currLine]; 
-
     if(containsSymbol(';',instruction)){
         string jmpBit = jmpToBit(instruction.substr(instruction.find(';')+1,instruction.size()-instruction.find(';')-2));
         binaryInstructionTemplate[14] = jmpBit[1];
@@ -220,64 +231,115 @@ void Cinstruction(int currLine, vector <string> & instructions, vector <string> 
     if(containsSymbol(';',instruction) && containsSymbol('=',instruction)){
         formula = instruction.substr(instruction.find('=')+1,instruction.find(';')-instruction.find('=')-1);
     }
-
     formula = cmpToBit(formula);
     
     for(int i =0;i< formula.size();i++){
         binaryInstructionTemplate[i+3] = formula[i];
     }
-
     binaryInstructions[currLine] = binaryInstructionTemplate;
 }
 
+void symbolMapping(map<string,int> & symbolTable, int currLine,string instruction){
+    char type = instruction[0];
+    if(type == '('){
+        instruction = instruction.substr(1,instruction.size()-3);
+        if(existsInMap(instruction, symbolTable)){
+            error = true;
+            errorMsg+="ima vec loop";
+        }
+        symbolTable[instruction] = currLine;
+    }
+    //counter za promenljive
+}
+
 int main(int argc, char* argv[])
-{	
+{    
+
     if(argc>2){
         cout << "Dont enter more than one file at a time!" << endl;
         return 1;
     }
 
-    string asmFileName = argv[1];
+    int lineCount = 0;
+    string currInstruction, asmFileName = argv[1];
     bool asmFileExists = false;
-
-	ifstream asmFile(asmFileName, ios::in);
+    vector <string> instructions, binaryInstructions;
+    ifstream asmFile(asmFileName, ios::in);
+    map<string, int> symbolTable;
+    for (int i = 0; i <= 15; i++) {
+        symbolTable["R" + to_string(i)] = i;
+    }
+    symbolTable["SP"] = 0;
+    symbolTable["LCL"] = 1;
+    symbolTable["ARG"] = 2;
+    symbolTable["THIS"] = 3;
+    symbolTable["THAT"] = 4;
+    symbolTable["SCREEN"] = 16384;
+    symbolTable["KBD"] = 24576;
+    
     if(!asmFile.good()){
         cout << "File doesnt exist!";
         return 1;
     }
-    ofstream hackFile(asmFileName.substr(0,asmFileName.size()-4)+".hack", ios::out);
-
-	int lineCount = 0;
-	string currInstructionRaw;
-	vector <string> instructions;
-	vector <string> binaryInstructions;
-	while(getline(asmFile, currInstructionRaw)){
-        string currInstruction = (currInstructionRaw);
-		if(!((currInstruction[0] == '/' && currInstruction[1] == '/')) && currInstruction.length()!=1){
-			lineCount++;
-			instructions.emplace_back(currInstruction);
-		}       
-	}
-	
-	binaryInstructions.resize(lineCount+1);
     
-	for(int i = 0;i < lineCount;i++){ 
-        if((instructions[i][0] == '(')){
-            
-        }
-        else if(instructions[i][0] == '@'){
-            if((instructions[i][1] >= '0' && instructions[i][1] <= '9')){
-			    Ainstruction(i, instructions, binaryInstructions);
-            }
-            else{
+    ofstream hackFile(asmFileName.substr(0,asmFileName.size()-4)+".hack", ios::out);
+    
+    while(getline(asmFile, currInstruction)){
 
+        currInstruction = removeWhiteSpace(currInstruction);
+
+        if(currInstruction==""){continue;}
+        
+        if(!((currInstruction[0] == '/' && currInstruction[1] == '/')) && (currInstruction.length()!=1)){
+            lineCount++;
+            instructions.emplace_back(currInstruction);
+        }  
+    }
+    
+    binaryInstructions.resize(lineCount+1);
+    
+    for(int i = 0;i < lineCount;i++){
+        string instruction = instructions[i];
+        if(instruction[0] == '('){
+	        symbolMapping(symbolTable,i, instruction);
+            lineCount--;
+            instructions.erase(instructions.begin() + i);
+
+        }                   
+    }
+    
+    for(int i = 0; i < lineCount;i++){
+        string instruction = instructions[i];
+        if((instruction[0] == '@') && (instruction[1] < '0' || instruction[1] > '9')){
+
+            if(!existsInMap(instruction.substr(1,instruction.size()-2), symbolTable)){
+                symbolTable.insert(make_pair(instruction.substr(1,instruction.size()-2),varCounter));
+                instructions[i] = "@" + to_string(varCounter);
+                varCounter++;
+            } 
+            else{
+                auto it = symbolTable.find(instruction.substr(1,instruction.size()-2));
+                instructions[i] = "@"+to_string(it->second);
             }
-		}
+        }   
+    }
+
+    for(int i = 0;i < lineCount;i++){
+        cout << instructions[i] << endl;
+    }
+
+    for(int i = 0;i < lineCount;i++){ 
+        if(error){
+            cout << errorMsg << endl;
+        }
+        if(instructions[i][0] == '@'){
+            Ainstruction(i, instructions, binaryInstructions);
+        }
         else{
             Cinstruction(i, instructions, binaryInstructions);
         }
+
     }
-
+    
     for(int i = 0;i < lineCount;i++){hackFile << binaryInstructions[i] <<  endl;}
-
 }
